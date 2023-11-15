c24f25218ec0c09ac7c3787ec6d770fae00f5dfc
Kork <hail3yp@gmail.com>
2023-11-15 18:23:53 -0500

    adding quality of life comments

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const FS = require('fs')
 const {Triangle, Circle, Square} = require('./lib/shapes.js');
 let shape;
 
+//Uses the shape list in the prompt to make the file
 function createLogo(answers) {
+    //Making variables out of our answers!
+
     let shapeColor = answers.shapeColor;
     let txt = answers.txt;
     let txtColor = answers.txtColor;
+
+    //Checking to see if the txt length matches the acceptance criteria
+
     if (txt.length > 3) {
         console.error("Text must be 3 characters or less!");
         return;
      }
+
+     //Returns the correct shape file. The classes have a render() method that renders the correct shape and pastes in the shapeColor, txt, and txtColor variables in as template literals.
+     
     switch (answers.shape){
         case "Triangle": 
             shape = new Triangle(shapeColor, txt, txtColor);
@@ -27,10 +36,11 @@ function createLogo(answers) {
             break;
 
     }
+    //renders the final shape and returns to the writeFile function!
     return shape.render()
 }
 
-//prompt for svg questions:
+//Asks the questions to gather all information:
 function questions() {
     INQUIRER.prompt([
         {
@@ -55,9 +65,11 @@ function questions() {
             name: 'shapeColor'
         }
     ]).then((answers) => {
+        //Sending to the createLogo Function to render answers
         const pageContent = createLogo(answers)
 
-        FS.writeFile('./examples/example.svg', pageContent, (err) =>
+        //Writing to a file in the output folder
+        FS.writeFile('./output/output.svg', pageContent, (err) =>
         err ? console.log(err) : console.log('Logo Created!'))
     })
 }
